61cf8dd1b1103399338ae750479e23fd805cb7ef
Oneil Williams <oneilswilliams@gmail.com>
2019-09-10 19:08:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, option|
     option.each do |type, name_array|
-      name_array.each do|name|
-        new_hash[name] = {
-          new_hash[attribute] 
-        }
+     
         binding.pry 
         
       end 
