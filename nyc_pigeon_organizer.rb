56f9c4db117cad33cf176512676c1aa47a72b888
Oneil Williams <oneilswilliams@gmail.com>
2019-09-10 19:14:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, option|
     option.each do |type, name_array|
-                   binding.pry
+
       if attribute == :gender
 
         name_array.each do |name|
